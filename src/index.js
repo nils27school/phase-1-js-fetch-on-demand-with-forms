73e18c586c1fd6264c66535b548cc82b24ce943c
nilssvensson <nils27school@gmail.com>
2022-09-06 10:32:08 -0400

    commit tree 73065430ef1765540afa4d25b494e5cb7cee72f3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
   
-}
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+  
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event);
+  });
+
+  
